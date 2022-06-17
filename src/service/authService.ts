43ba138116de22d25e170bfa2d57b85c272431ba
yashiroryo <>
2022-06-17 17:09:12 +0900

    fix: tokenがlocalStorageにない場合に再度ログインページに遷移するように変更

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -9,7 +9,7 @@ import axios from "../config/axiosConfig";
 export default function signin() {
   const token = localStorage.getItem("data-user-token");
   //const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjExLCJ1c2VyTmFtZSI6IuW-s-WztuWkqumDjiIsInNldHRpbmciOnsiaXNEZWxldGVNb2RhbFNob3ciOjF9LCJpYXQiOjE2NTQ5NTg4ODZ9.3QRqu_-DMA-nlhKvx39-ihsacMb0mKVhXMO5j0nE4no";
-  if (token != null) {
+  if (token?.length != 0 && token != null) {
     setTimeout(() => {
       console.log("token exists local storage");
       setUserToken(token);
